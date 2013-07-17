5ca5b7e647b45822d5d1967585fe2a5e7c69817e
jgoodIDTR <joseph.good@interactivedata.com>
2013-07-17 13:45:32 -0500

    Modified the file to pull from Win32::Registry
    The previous version, pulling from WMI, was incredibly slow, ranging from 20s on one of my faster machines to approx. 6 minutes on one of my slower ones. I have yet to have this result in a time over 2s.
    
    The downside to this version is that it is noticeably messier, the formatting does not look as nice and some files do not seem to have all attributes stored.

--- win32_software.rb
+++ win32_software.rb
@@ -1,14 +1,16 @@
-require 'ruby-wmi'
-
 provides 'installed_software'
 
 installed_software Mash.new
 
-apps = WMI::Win32_Product.find(:all)
-
-apps.each do |app|
-  installed_software[app.packagename] = Mash.new
-  %w(name version vendor packagenameckagename installdate).each do |attrib|
-    installed_software[app.packagenameename][attrib] = app.send(attrib)
+Win32::Registry::HKEY_LOCAL_MACHINE.open('Software\Microsoft\Windows\CurrentVersion\Uninstall') do |reg|
+  reg.each_key do |key|
+    k = reg.open(key)
+    installed_software[(k["DisplayName"] rescue "?")] = Mash.new
+    installed_software[(k["DisplayName"] rescue "?")]['DisplayVersion'] = k["DisplayVersion"] rescue "?"
+    installed_software[(k["DisplayName"] rescue "?")]['Version'] = k["Version"] rescue "?"
+    installed_software[(k["DisplayName"] rescue "?")]['Publisher'] = k["Publisher"] rescue "?"
+    installed_software[(k["DisplayName"] rescue "?")]['InstallDate'] = k["InstallDate"] rescue "?"
+    installed_software[(k["DisplayName"] rescue "?")]['InstallLocation'] = k["InstallLocation"] rescue "?"
+    installed_software[(k["DisplayName"] rescue "?")]['URLInfoAbout'] = k["URLInfoAbout"] rescue "?"
   end
 end
